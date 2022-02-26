b1f19b9993ffe164fe5e8da9f0e92dad64f504ef
AndyYulianto11 <andyyulianto40@gmail.com>
2022-02-26 16:23:22 +0700

    update dashboard

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\MahasiswaController;
+use App\Http\Controllers\AdminController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -19,3 +20,4 @@ Route::get('/', [MahasiswaController::class, 'index']);
 Route::get('/mahasiswa/login/', [LoginController::class, 'index'])->name('login');
 Route::post('/mahasiswa/login/', [LoginController::class, 'login'])->name('login.custom');
 Route::get('/logout', [LoginController::class, 'logout'])->name('logout');
+Route::get('/admin/', [AdminController::class, 'index'])->name('admin');
